35d8ebbd5e197a169b8a12a4e38d58460d1323b3
joão marcos ferreira dos santos lucas <joaowebdeveloper@gmail.com>
2024-01-24 00:41:07 -0300

    Create index.php

---
+++ class02/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>aula 02</title>
+</head>
+<body>
+    <?php 
+    $nome = "joão";
+    $sobrenome = "marcos";
+
+    echo "muito prazer meu nome é $nome $sobrenome" 
+    ?>
+</body>
+</html>
